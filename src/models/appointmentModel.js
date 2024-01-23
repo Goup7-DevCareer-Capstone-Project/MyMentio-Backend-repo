a976dc3f5d27e75d618e471bcbe0bebf6611c3cf
Amaka Ibenegbu <113144586+AmakaJuliet@users.noreply.github.com>
2024-01-23 07:08:51 +0100

    commit

--- src/models/appointmentModel.js
+++ src/models/appointmentModel.js
@@ -1,55 +1,56 @@
-const mongoose = require("mongoose");
+const { Schema, SchemaType, } = require("mongoose");
 
-const appointmentSchema= new mongoose.Schema(
-    {
-        patient: {
-            type: Schema.Types.ObjectId,
-            ref:"User",
-          required:true
-        },
-        therapist: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Therapist',
-            required: true
-        },
-        date: { 
-            type: Date,
-            required: true
-        },
-        time: {
-            type: String, 
-            required: true
-        },
+const ApiError = require("../utils/ApiError");
+
+const appointmentSchema = new mongoose.Schema(
+  {
+    patient: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
     },
-    {timestamps: true}
+    therapist: {
+      type: Schema.Types.ObjectId,
+      ref: "Therapist",
+      required: true,
+    },
+    date: {
+      type: Date,
+      required: true,
+    },
+    time: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
 );
 
-appointmentSchema.pre('save',async function(next){
-    try {
-        const userAppointment = await this.constructor.findOne({
-            therapist: this.therapist,
-            date: this.date,
-            time: this.time,
-        })
+appointmentSchema.pre("save", async function (next) {
+  try {
+    const userAppointment = await this.constructor.findOne({
+      therapist: this.therapist,
+      date: this.date,
+      time: this.time,
+    });
 
-        const therapistAppointment = await this.constructor.findOne({
-            patient: this.patient,
-            date: this.date,
-            time: this.time,
-        });
+    const therapistAppointment = await this.constructor.findOne({
+      patient: this.patient,
+      date: this.date,
+      time: this.time,
+    });
 
-        if(userAppointment || therapistAppointment ){
-            throw new ApiError("Appointment time is not available. Please choose a different time.")
-        }
-
-        next()
-
-    } catch (error) {
-        next(error)   
+    if (userAppointment || therapistAppointment) {
+      throw new ApiError(
+        "Appointment time is not available. Please choose a different time."
+      );
     }
-})
 
-
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
 
 const Appointment = mongoose.model("Appointment", appointmentSchema);
-module.exports = Appointment
+module.exports = Appointment;
