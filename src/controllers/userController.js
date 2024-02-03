bef9562889c76ececdaaa1ee2f531c202e3ff236
Amaka Ibenegbu <113144586+AmakaJuliet@users.noreply.github.com>
2024-02-03 02:34:56 +0100

    commit

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -56,7 +56,7 @@ const registerUser = asyncHandler(async (req, res) => {
       const text = `<h1>Email Confirmation</h1>
         <h2>Hello ${firstName}</h2>
         <p>Verify your email address to complete the signup and login to your account to My Mentio</p>
-        <a href='link/api/user/register/${user.verificationCode}'> Click here</a> 
+        <a href='https://mymentio-backend.onrender.com/api/user/register/${user.verificationCode}'> Click here</a> 
 
         </div>`;
 
@@ -335,7 +335,7 @@ const forgotPassword = asyncHandler(async (req, res) => {
         <h2>Hello ${user.firstName}</h2>
         <p>You are receiving this email because you (or someone else) has
          requested the reset of a password</p>
-          <a href='link/api/user/resetpassword/${resetToken}'> Click here to reset your password</a> 
+          <a href='https://mymentio-backend.onrender.com/api/user/resetpassword/${resetToken}'> Click here to reset your password</a> 
 
         </div>`;
 
